1e749480a5ab9c5c25a9ab163878e08019bbc480
Bryan Gordon <bryanbge92@gmail.com>
2025-08-19 19:23:52 -0500

    Add function examples and usage in main.go
    Added commented examples of function definitions, error handling, variadic functions, closures, and custom types in Go. Updated main() to demonstrate usage of these functions, including division with error handling, a closure-based counter, and a custom type with a method.

--- first-go/main.go
+++ first-go/main.go
@@ -1,8 +1,11 @@
 package main //Siempre hay que traer el paquete main, tambien define el punto de entrada
 import (
 	"fmt"
-	// "strings"
 )
+
+/* Package a importar*/
+// errors
+// "strings"
 
 // Este es el punto de entrada de la aplicación
 
@@ -10,6 +13,61 @@ import (
 // byte === unit8 | para trabajar con datos binarios
 // rune === int32 | para representar un solo caracter que ocupa mas de un byte
 // complex64, complex128 | valores que tienen una parte real y otra imaginaria
+// nil === null
+
+/* Funciones
+
+func suma(a int, b int) int { // Siempre hay que colocar el tipo de dato que retorna
+	result := a + b
+	return result
+}
+
+func suma2(a, b int) int { // Otra forma de simplificar los tipos de datos en los parametros
+	return a + b
+}
+
+func division(a, b float64) (float64, float64) { // Funcion que devuelve mas de un valor
+	cociente := a / b
+	resto := float64(int(a) % int(b))
+
+	return cociente, resto
+}
+
+func divisionError(a, b float64) (float64, error) { // Funcion que es capaz de manejar errores, el error siempre va al final
+	if b == 0 {
+		fmt.Errorf("No se puede dividir para 0")                      // Warning por usar errors.New(), se tendria que utilizar uno de los dos y retornarlo
+		return 0, errors.New("ha ocurrido un error al dividir por 0") // erros.New() crea errores; hay que pasar como primer valor un 0, porque es el valor por defecto.
+	}
+
+	cociente := a / b
+
+	return cociente, nil // retornamos nil ya que no hemos tenido ningun error
+}
+
+func showNombres(nombres ...string) { // Funcion de parametros variables, los 3 puntos indican que no se sabe el numero total de parametros
+	for _, nombres := range nombres {
+		fmt.Println(nombres)
+	}
+}
+
+func contador() func() int { // Funcion de clausura (funcion que retorna otra funcion)
+	cont := 0
+
+	return func() int {
+		cont++
+		return cont
+	}
+}
+
+type rectangulo struct { // Forma de definir tipos; Las clases no existen en go, se tiene que crear un tipo y luego una funcion asociada
+	ancho, alto float64
+}
+
+func (rec rectangulo) area() float64 { // El primer parentesisi representa que esta asociado a un type o interface
+	return rec.alto * rec.ancho
+}
+
+*/
 
 func main() {
 	/* Basics
@@ -100,12 +158,31 @@ func main() {
 
 	*/
 
-	/* Funciones */
+	/* Declaracion y utilizacion de funciones */
 
-}
+	cociente, resto := division(20, 2) // Cada variable obtiene el valor que retorna la funcion respectivamente
 
-func suma(a int, b int) {
-	result := a + b
-	fmt.Println("Alog")
-	return result
+	cocienteError, error := divisionError(10, 3) // Se hace de las misma manera pero despues hay que controlar el error
+	cocienteError2, _ := divisionError(10, 3)    // Se puede evitar usar el error
+
+	if error != nil {
+		// panic("La funcion divisionError produjo un error") // La funcion panic() cierra aborta completamente la ejecucion del programa
+		fmt.Printf("Hubo un error %s", error)
+		return
+	}
+
+	fmt.Println("Division", cociente, resto)
+	fmt.Println("Division con error completa", cocienteError)
+	fmt.Println("Division con error solo resultado", cocienteError2)
+
+	conta := contador()               // La funcion se ejecuta sin mas
+	fmt.Println("Contador:", conta()) // Hay que usar la variable creada anteriormente como si fuera una funcion
+	fmt.Println("Contador:", conta())
+	fmt.Println("Contador:", conta())
+	fmt.Println("Contador:", conta())
+	fmt.Println("Contador:", conta())
+
+	rectan := rectangulo{alto: 10, ancho: 5} // Definir primero los valores en el type/interface
+	fmt.Println(rectan.area())               // La variable tiene acceso a las funciones del type/interface
+
 }
