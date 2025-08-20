a3fceedc91b29624ae9aa20270195af0f252ff2e
Bryan Gordon <bryanbge92@gmail.com>
2025-08-19 23:44:28 -0500

    Add examples for interfaces and concurrency in Go
    Added commented code examples demonstrating the use of interfaces, GoRoutines, channels, and mutexes in Go. These examples illustrate how to implement polymorphism, concurrent execution, and safe data access across multiple goroutines.

--- first-go/main.go
+++ first-go/main.go
@@ -80,6 +80,52 @@ func incrementarValor(value *int) { // Para poder acceder al argumento en memori
 // new() permite crear un puntero con un valor predeterminado del tipo correspondiente
 */
 
+/* Interfaces
+// No se implementan (al menos manualmente)
+// Se cumplen
+
+type forma interface {
+	area() float64
+}
+
+type circle struct {
+	radio float64
+}
+
+func (cir circle) area() float64 {
+	return math.Pi * cir.radio * cir.radio
+}
+
+func imprimirArea(f forma) {
+	fmt.Printf("El area es: %.2f\n", f.area()) // %.2f significa que solo va a utilizar dos decimales
+}
+
+var cualquiera interface{} // Forma de declarar una interfaz que sea cualquier cosa/generica; como un type any (TS)
+
+*/
+
+/* Concurrencia y polimorfismo
+// Todo el codigo se ejecuta de forma sincrona
+// Se usa la palabra reservada go
+// Al usar la palabra go al lado de un codigo se crea una GoRoutine
+// GoRoutine es un hilo de ejecucion ligero virtual
+// Channel permite comunicar cosas entre GoRoutines
+
+func saludar(canal chan<- string) { // Tipo chan define el Channel y la flecha indica que recibe un string (importante definir bien el tipo de dato)
+	time.Sleep(1 * time.Second)        // Se ejecuta cada segundo
+	canal <- "Hola desde la GoRoutine" // Se envia el string hacia el canal
+}
+
+func imprimirSaludo(canal <-chan string) { // Asi se recibe un dato desde la GoRoutine
+	fmt.Println("Esperando mensaje...")
+	mensaje := <-canal
+	fmt.Println(mensaje)
+}
+
+*/
+
+/* Packages */
+
 func main() {
 	/* Basics
 	fmt.Println("Hola go")
@@ -100,6 +146,7 @@ func main() {
 	sliceVariable = append(sliceVariable, 7) // append sirve para agregar elementos
 	fmt.Println(len(arrayFijo))              // len muestra cual es el total de elementos del array
 	fmt.Println(cap(arrayFijo))              // cap() Muestra cual es la capacidad total del array
+	otherSlice := make([]int, 5, 10) 				// Permite crear slices con una longuitud y una capacidad respectivamente, con un solo parametro crea la misma longuitud y capacidad
 
 	// Maps
 	diccionario := map[string]int{ // Se typa primero la clave "string" y luego el tipo de dato de los valores "int"
@@ -211,4 +258,62 @@ func main() {
 	*puntero = 20
 	fmt.Println("Valor de memoria con new, cambiado:", *puntero)
 	*/
+
+	/* Uso de interfaces
+	circulo := circle{radio: 5}
+
+	imprimirArea(circulo) // La funcion infiere que circulo es una forma porque poseen un metodo con el mismo nombre, al ser diferente marcaria un error; el nombre debe ser exacto
+	*/
+
+	/* Uso de Concurrencia, polimorfismo
+	nuevoCanal := make(chan string) // Con make se crea un canal
+	go saludar(nuevoCanal)          // Se ejecuta la GoRoutine
+	imprimirSaludo(nuevoCanal)
+
+	otroCanal := make(chan int)
+	go func() { // Se crea una GoRoutine que ejecuta una funcion
+		for i := 0; i < 3; i++ {
+			otroCanal <- i
+		}
+		close(otroCanal) // Se tiene que cerrar el canal
+	}() // Se coloca () para ejecutar la funcion
+
+	for num := range otroCanal { // Se puede utilizar los valores ejecutados desde la GoRutine/Channel
+		fmt.Println("Valor recibido desde la GoRutine:", num)
+	}
+
+	// Un canal es Writer y Reader ya que se puede leer y escribir datos (recibir y enviar)
+	// Un ejemplo de solo Writer es range
+
+	// Ejemplo con uso de mutex
+	// Mutex permite reserva recursos cuando un mismo canal se esta ejecutando mas de una vez
+	var contar int
+	var mu sync.RWMutex // Es un lector y escritor de mutex (Reader/Writer)
+
+	// Writer Elemento que permite escribir/enviar datos
+	go func() {
+		for i := 0; i < 3; i++ {
+			mu.Lock()   // Bloquea el codigo que se ejecuta dentro de la funcion
+			contar++    // No se va a poder utilizar "contar" hasta que la ejecucion no termine
+			mu.Unlock() // Desbloquea el uso del codigo posterior
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	// Reader Elemento que permite escribir/recibir datos
+	for i := 0; i < 3; i++ {
+		go func(id int) {
+			for j := 0; j < 5; j++ {
+				mu.RLock() // Permite bloquear la lectura
+				fmt.Printf("Leyendo desde la GoRoutine %d: %d\n", id, contar)
+				mu.RUnlock() // Permite desbloquear la lectura
+				time.Sleep(100 * time.Millisecond)
+			}
+		}(i)
+	}
+	time.Sleep(2 * time.Second)
+	// En consola se ve como se repiten los datos, eso es porque estan bloqueados
+
+	*/
+
 }
