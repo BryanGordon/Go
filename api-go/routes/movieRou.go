f00a209e5aa468208d9e90d52a752e2f3c0b57d1
Bryan Gordon <bryanbge92@gmail.com>
2025-09-08 21:49:33 -0500

    Add list functionality

--- api-go/routes/movieRou.go
+++ api-go/routes/movieRou.go
@@ -1,10 +1,19 @@
 package routes
 
 import (
+	"api-go/data"
+	"api-go/models"
+	"encoding/json"
 	"net/http"
 )
 
 func GetMoviesHandler(res http.ResponseWriter, req *http.Request) {
+	var movies []models.Movie
+
+	data.DB.Table("movie").Find(&movies)
+
+	json.NewEncoder(res).Encode(&movies)
+
 	res.Write([]byte("get movies"))
 }
 
