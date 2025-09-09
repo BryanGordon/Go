d05ee758b06d3dc57d43792553e561b94dc4fcb7
Bryan Gordon <bryanbge92@gmail.com>
2025-09-08 21:48:40 -0500

    Delete unused code

--- api-go/models/movie.go
+++ api-go/models/movie.go
@@ -1,11 +1,11 @@
 package models
 
 type Movie struct {
-	Id       uint    `json:"id"`
-	Title    string  `json:"title"`
-	Years    int     `json:"years"`
-	Director string  `json:"director"`
-	Duration int     `json:"duration"`
-	Poster   string  `json:"poster"`
-	Rate     float64 `json:"rate"`
+	Id       string
+	Title    string
+	Years    int
+	Director string
+	Duration int
+	Poster   string
+	Rate     float64
 }
