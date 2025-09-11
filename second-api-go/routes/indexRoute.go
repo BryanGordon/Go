00951579ce1081fa92a496c106701aaf1f71bda1
Bryan Gordon <bryanbge92@gmail.com>
2025-09-10 21:46:01 -0500

    Add products routes

--- second-api-go/routes/indexRoute.go
+++ second-api-go/routes/indexRoute.go
@@ -4,6 +4,11 @@ import "github.com/gorilla/mux"
 
 func GetRoutes() *mux.Router {
 	routes := mux.NewRouter()
+	routes.HandleFunc("/", GetProducts).Methods("GET")
+	routes.HandleFunc("/add", AddProduct).Methods("POST")
+	routes.HandleFunc("/{id}", UpdateProduct).Methods("GET")
+	routes.HandleFunc("/search", SearchProduct).Methods("POST")
+	routes.HandleFunc("/{id}", DeleteProduct).Methods("DELETE")
 
 	return routes
 }
