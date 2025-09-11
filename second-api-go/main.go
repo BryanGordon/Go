3c136cdf62e6a6715dc9318a380a955035e2d349
Bryan Gordon <bryanbge92@gmail.com>
2025-09-10 21:45:13 -0500

    Add db connection

--- second-api-go/main.go
+++ second-api-go/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"second-api-go/db"
 	"second-api-go/routes"
 )
 
 func main() {
+	db.DbConnection()
 	data := routes.GetRoutes()
 
 	log.Fatal(http.ListenAndServe(":3000", data))
