8c544a446e840c504d4b21b3710e4328b50be2b7
Bryan Gordon <bryanbge92@gmail.com>
2025-09-12 20:00:43 -0500

    Create user model

--- second-api-go/models/userModel.go
+++ second-api-go/models/userModel.go
@@ -1,1 +1,11 @@
 package models
+
+import "github.com/google/uuid"
+
+type User struct {
+	Id       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Apellido string    `json:"apellido"`
+	Correo   string    `json:"correo"`
+	Rol      string    `json:"rol"`
+}
