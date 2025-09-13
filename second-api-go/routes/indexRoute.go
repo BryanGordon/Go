b7f75197c2d9a71e1e6c587665b41edf31c6e3af
Bryan Gordon <bryanbge92@gmail.com>
2025-09-12 20:01:01 -0500

    Create routes for users data

--- second-api-go/routes/indexRoute.go
+++ second-api-go/routes/indexRoute.go
@@ -10,5 +10,11 @@ func GetRoutes() *mux.Router {
 	routes.HandleFunc("/search/{name}", SearchProduct).Methods("GET")
 	routes.HandleFunc("/{name}", DeleteProduct).Methods("DELETE")
 
+	routes.HandleFunc("/users", GetUsers).Methods("GET")
+	routes.HandleFunc("/users/search/{name}", SearchUser).Methods("GET")
+	routes.HandleFunc("/users/{id}", UpdateRolUser).Methods("PATCH")
+	routes.HandleFunc("/users/{id}", UpdateNameUser).Methods("PATCH")
+	routes.HandleFunc("/users/{name}", DeleteUser).Methods("DELETE")
+
 	return routes
 }
