cc0494e19bead262fa092042b7e209e9a11439a5
Bryan Gordon <bryanbge92@gmail.com>
2025-09-23 20:54:54 -0500

    Update to CORS handler

--- second-api-go/main.go
+++ second-api-go/main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 	"second-api-go/db"
 	"second-api-go/routes"
+
+	"github.com/gorilla/handlers"
 )
+
+var allowedOri = []string{"*"}
+var allowedMeth = []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"}
+var allowedHeaders = []string{"Content-Type", "Authorization"}
 
 func main() {
 	db.DbConnection()
 	data := routes.GetRoutes()
 
-	log.Fatal(http.ListenAndServe(":3000", data))
+	log.Fatal(http.ListenAndServe(":3000",
+		handlers.CORS(handlers.AllowedOrigins(allowedOri), handlers.AllowedMethods(allowedMeth), handlers.AllowedHeaders(allowedHeaders))(data)))
 }
