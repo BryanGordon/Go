b2aca23f683639c97a2b83863a5e817de6056777
Bryan Gordon <bryanbge92@gmail.com>
2025-09-30 20:59:42 -0500

    Create methods for Supabase queries

---
+++ second-api-go/routes/productRouteSupa.go
@@ -0,0 +1,19 @@
+package routes
+
+import "net/http"
+
+func GetDataSupa(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func CreateProductSupa(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func UpdateProductSupa(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func DeleteProductSupa(res http.ResponseWriter, req *http.Request) {
+
+}
