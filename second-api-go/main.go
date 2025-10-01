b58dc8cf1cfc01658c4846354a8c4b67f7606b9b
Bryan Gordon <bryanbge92@gmail.com>
2025-09-30 20:59:11 -0500

    Add connection to entry file

--- second-api-go/main.go
+++ second-api-go/main.go
@@ -14,9 +14,14 @@ var allowedMeth = []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"}
 var allowedHeaders = []string{"Content-Type", "Authorization"}
 
 func main() {
+	/* Conexion con local db mysql */
 	db.DbConnection()
+	/* Conexion con supabase */
+	db.SupaConnect()
+
 	data := routes.GetRoutes()
 
 	log.Fatal(http.ListenAndServe(":3000",
 		handlers.CORS(handlers.AllowedOrigins(allowedOri), handlers.AllowedMethods(allowedMeth), handlers.AllowedHeaders(allowedHeaders))(data)))
+
 }
