db192dc9780a07b9219b04bbfde76d0a25b2b0b4
Bryan Gordon <bryanbge92@gmail.com>
2025-09-30 20:58:32 -0500

    Create a connection with supabase

---
+++ second-api-go/db/supaDB.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"log"
+
+	"github.com/supabase-community/supabase-go"
+)
+
+var api_url = ""
+var api_key = ""
+var SupaCli *supabase.Client
+
+func SupaConnect() {
+	var err error
+	SupaCli, err = supabase.NewClient(api_url, api_key, &supabase.ClientOptions{})
+
+	if err != nil {
+		log.Fatal("No se ha podido conectar a supabase... ", err)
+	} else {
+		log.Print("Se ha conectado a supabase...")
+	}
+
+}
