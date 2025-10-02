a4508a2b3b74d0e48388b33d2f1e6141ef0149cf
Bryan Gordon <bryanbge92@gmail.com>
2025-10-01 21:27:51 -0500

    Add Supabase user route handlers
    Implemented GetUsersSupa and SearchUserSupa functions to fetch all users and search users by name from the Supabase 'users' table. Added placeholders for CreateUserSupa, UpdateUserSupa, and DeleteUserSupa route handlers.

--- second-api-go/routes/userRouteSupa.go
+++ second-api-go/routes/userRouteSupa.go
@@ -1,1 +1,53 @@
 package routes
+
+import (
+	"net/http"
+	"second-api-go/db"
+
+	"github.com/gorilla/mux"
+)
+
+func GetUsersSupa(res http.ResponseWriter, req *http.Request) {
+	data, _, err := db.SupaCli.From("users").Select("*", "", false).Execute()
+
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Data not found"))
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	res.Write(data)
+}
+
+func SearchUserSupa(res http.ResponseWriter, req *http.Request) {
+	var param = mux.Vars(req)
+	data, _, err := db.SupaCli.From("users").Select("*", "", false).Eq("name", param["name"]).Execute()
+
+	/* Si el usuario no existe devuelve un array vacio, arreglar eso*/
+	/* Probar si la api key publica se conecta y hace las mismas funciones que la actual*/
+	/* Probar la primera api key generica, tal vez funciona*/
+
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("User not found"))
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	res.Write(data)
+}
+
+func CreateUserSupa(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func UpdateUserSupa(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func DeleteUserSupa(res http.ResponseWriter, req *http.Request) {
+
+}
