ecf56f8526af233c2a86225854d6a7c40808e5d3
Bryan Gordon <bryanbge92@gmail.com>
2025-10-01 21:27:30 -0500

    Add get products data logic

--- second-api-go/routes/productRouteSupa.go
+++ second-api-go/routes/productRouteSupa.go
@@ -1,9 +1,22 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+	"second-api-go/db"
+)
 
 func GetDataSupa(res http.ResponseWriter, req *http.Request) {
+	data, _, err := db.SupaCli.From("products").Select("*", "", false).Execute()
 
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Data not found"))
+		return
+	}
+
+	res.Header().Set("Content-type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	res.Write(data)
 }
 
 func CreateProductSupa(res http.ResponseWriter, req *http.Request) {
