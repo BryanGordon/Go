ed3ca09c8f537cc89a2aad379baf55705a04b15b
Bryan Gordon <bryanbge92@gmail.com>
2025-10-07 21:17:22 -0500

    Add search function for products

--- second-api-go/routes/productRouteSupa.go
+++ second-api-go/routes/productRouteSupa.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"net/http"
 	"second-api-go/db"
+
+	"github.com/gorilla/mux"
 )
 
 func GetDataSupa(res http.ResponseWriter, req *http.Request) {
@@ -15,6 +17,21 @@ func GetDataSupa(res http.ResponseWriter, req *http.Request) {
 	}
 
 	res.Header().Set("Content-type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	res.Write(data)
+}
+
+func SearchProductSupa(res http.ResponseWriter, req *http.Request) {
+	param := mux.Vars(req)
+
+	data, _, err := db.SupaCli.From("products").Select("*", "", false).Eq("name", param["name"]).Execute()
+
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Product not found"))
+		return
+	}
+
 	res.WriteHeader(http.StatusOK)
 	res.Write(data)
 }
