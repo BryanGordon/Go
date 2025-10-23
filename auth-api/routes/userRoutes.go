68468d098b244968e4d92aa91ec1ba7b3e18f986
Bryan Gordon <bryanbge92@gmail.com>
2025-10-22 22:12:36 -0500

    Create methods for user management

---
+++ auth-api/routes/userRoutes.go
@@ -0,0 +1,27 @@
+package routes
+
+import "net/http"
+
+func UserList(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func AddUser() {
+
+}
+
+func UpdateUserName() {
+
+}
+
+func UpdateUserMail() {
+
+}
+
+func UpdateUserNick() {
+
+}
+
+func RemoveUser() {
+
+}
