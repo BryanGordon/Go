3218ae98a5ff0037c92a8fc91205c5444fd1adba
Bryan Gordon <bryanbge92@gmail.com>
2025-10-22 22:07:36 -0500

    Create methods for books management

---
+++ auth-api/routes/bookRoutes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"net/http"
+)
+
+func ListBooks(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func AddBooks(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func UpdateNameBooks(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func UpdateBookPages(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func UpdateBookAuthor(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func RemoveBook(res http.ResponseWriter, req *http.Request) {
+
+}
