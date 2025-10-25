7acdcdee74bba37e9d0e86f3ef7317d264ea8a48
Bryan Gordon <bryanbge92@gmail.com>
2025-10-24 20:57:27 -0500

    Create users model

--- auth-api/models/users.go
+++ auth-api/models/users.go
@@ -1,1 +1,10 @@
 package models
+
+type Users struct {
+	Id         string `json:"id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	CreatedDay string `json:"created-day"`
+	Nickname   string `json:"nickname"`
+	Password   string `json:"password"`
+}
