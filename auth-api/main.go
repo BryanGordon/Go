3622bd3ba1bf8fef5ebe77d0856150963c368733
Bryan Gordon <bryanbge92@gmail.com>
2025-10-27 20:47:10 -0500

    Add CORS policies and use env variables

--- auth-api/main.go
+++ auth-api/main.go
@@ -5,13 +5,22 @@ import (
 	"auth-api/routes"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	connections.SupaConnec()
 
 	routes := routes.GetRoutes()
 
-	log.Fatal(http.ListenAndServe(":3000", routes))
+	log.Fatal(http.ListenAndServe(":3000",
+		handlers.CORS(
+			handlers.AllowedMethods([]string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"}),
+			handlers.AllowedOrigins([]string{"http://localhost:4321"}),
+			handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+		)(routes)))
 
 }
