6ec552d4013769a936bdb0605ffa2c31758490f6
Bryan Gordon <bryanbge92@gmail.com>
2025-10-27 20:47:38 -0500

    Create endpoints for api

--- auth-api/routes/indexRoutes.go
+++ auth-api/routes/indexRoutes.go
@@ -7,5 +7,20 @@ import (
 func GetRoutes() *mux.Router {
 	routes := mux.NewRouter()
 
+	routes.HandleFunc("/", UserList).Methods("GET")
+	routes.HandleFunc("/", AddUser).Methods("POST")
+	routes.HandleFunc("/search", SearchUser).Methods("GET")
+	routes.HandleFunc("/update-name", UpdateUserName).Methods("PATCH")
+	routes.HandleFunc("/update-email", UpdateUserMail).Methods("PATCH")
+	routes.HandleFunc("/update-nick", UpdateUserNick).Methods("PATCH")
+	routes.HandleFunc("/", RemoveUser).Methods("DELETE")
+
+	routes.HandleFunc("/books", ListBooks).Methods("GET")
+	routes.HandleFunc("/books", AddBooks).Methods("POST")
+	routes.HandleFunc("/search/books", SearchBook).Methods("GET")
+	routes.HandleFunc("/books/update-name", UpdateNameBooks).Methods("PATCH")
+	routes.HandleFunc("/books/update-author", UpdateBookAuthor).Methods("PATCH")
+	routes.HandleFunc("/books/update-", UpdateBookPages).Methods("PATCH")
+	routes.HandleFunc("/books", RemoveBook).Methods("DELETE")
 	return routes
 }
