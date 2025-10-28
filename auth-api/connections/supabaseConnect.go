83c41c6838ded0a8de9f7fe34f12eed7487717df
Bryan Gordon <bryanbge92@gmail.com>
2025-10-27 20:46:06 -0500

    Add env variables for connections

--- auth-api/connections/supabaseConnect.go
+++ auth-api/connections/supabaseConnect.go
@@ -2,6 +2,7 @@ package connections
 
 import (
 	"log"
+	"os"
 
 	"github.com/supabase-community/supabase-go"
 )
@@ -10,10 +11,12 @@ var Client *supabase.Client
 
 func SupaConnec() {
 	var err error
-	Client, err = supabase.NewClient("", "", &supabase.ClientOptions{})
+	api_url := os.Getenv("API_URL")
+	api_key := os.Getenv("API_KEY")
+	Client, err = supabase.NewClient(api_url, api_key, &supabase.ClientOptions{})
 
 	if err != nil {
-		log.Fatal("No se ha podido conectar al servidor")
+		log.Fatal("No se ha podido conectar al servidor", err)
 	} else {
 		log.Print("Conectado a supabase")
 	}
