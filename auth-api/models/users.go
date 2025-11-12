bfbb60fd648db151cf35e44b7f591cfd1daba0eb
Bryan Gordon <bryanbge92@gmail.com>
2025-11-11 20:07:06 -0500

    Add Rol field to user model

--- auth-api/models/users.go
+++ auth-api/models/users.go
@@ -5,6 +5,7 @@ import "github.com/google/uuid"
 type Users struct {
 	Id          uuid.UUID `json:"id"`
 	Name        string    `json:"name"`
+	Rol         string    `json:"rol"`
 	Email       string    `json:"email"`
 	CreatedUser string    `json:"createdUser"`
 	Nickname    string    `json:"nickname"`
