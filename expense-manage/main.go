2e926769d446abd2ce7487e712f3a92d2c5dc4f8
Bryan Gordon <bryanbge92@gmail.com>
2025-12-24 20:48:06 -0500

    Add some code

--- expense-manage/main.go
+++ expense-manage/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	const input = ""
+
+	if input == "exit" {
+		fmt.Println("Saliendo de la aplicacion...")
+	}
+	if input == "list" {
+		fmt.Println("Enseñar lista")
+	}
+
+	if input == "add" {
+		fmt.Println("Agregando gastos")
+	}
 
 }
