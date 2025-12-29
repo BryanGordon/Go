c225edefa89de2bf3f81b2cfe26e8c9fc991a1c1
Bryan Gordon <bryanbge92@gmail.com>
2025-12-29 18:59:00 -0500

    Add initial expense struct and file handling setup
    Introduced the Expensives struct and a placeholder for file creation logic. Updated main to use bufio for input, added startup and exit messages, and included basic input handling.

--- expense-manage/main.go
+++ expense-manage/main.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+var fileName = "expensives-list"
+
+type Expensives struct {
+	Id          string `json:"id"`
+	Date        string `json:"date"`
+	Description string `json:"description"`
+	Amount      int64  `json:"amount"`
+}
+
+func createFile(newData Expensives) {
+	var expensives []Expensives
+}
 
 func addExpense() {
 
@@ -11,11 +30,17 @@ func listExpenses() {
 }
 
 func main() {
-	const input = ""
+	var input = ""
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Iniciando app..")
+	input, _ = reader.ReadString('\n')
+	input = strings.TrimSpace(input)
 
 	for {
 		if input == "exit" {
 			fmt.Println("Saliendo de la aplicacion...")
+			time.Sleep(1 * time.Second)
 			break
 		}
 
