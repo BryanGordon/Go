bedef89884f3a8a8fa488394f9fca8ebefc79469
Bryan Gordon <bryanbge92@gmail.com>
2025-12-30 19:01:28 -0500

    Refactor createFile to use JSON for expense storage
    Updated the createFile function to read and write expenses as JSON, enabling persistent storage of expense data. Added logic to check for file existence, handle JSON marshaling/unmarshaling, and print appropriate messages. Also added a call to createFile in the 'list' command, though the usage of newData in this context may need review.

--- expense-manage/main.go
+++ expense-manage/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -19,6 +20,39 @@ type Expensives struct {
 
 func createFile(newData Expensives) {
 	var expensives []Expensives
+	isExist := false
+
+	_, err := os.Stat(fileName)
+
+	if err == nil {
+		data, err := os.ReadFile(fileName)
+		isExist = true
+
+		if err == nil && len(data) > 0 {
+			_ = json.Unmarshal(data, &expensives)
+		}
+	}
+
+	expensives = append(expensives, newData)
+	newJson, err := json.MarshalIndent(expensives, "", " ")
+
+	if err != nil {
+		fmt.Println("An error ocurred creating the json file")
+		panic(err)
+	}
+
+	err = os.WriteFile(fileName, newJson, 0644)
+
+	if err != nil {
+		fmt.Println("Error ocurred writing the file")
+		panic(err)
+	}
+
+	if isExist {
+		fmt.Println("Se ha agregado un gasto..")
+	} else {
+		fmt.Println("Archivo creado exitosamente..")
+	}
 }
 
 func addExpense() {
@@ -45,7 +79,9 @@ func main() {
 		}
 
 		if input == "list" {
+			newData := []Expensives
 			fmt.Println("Enseñar lista")
+			createFile(newData)
 		}
 
 		if input == "add" {
