36bf23c80172815daf5ac7f246068d02d6371cc8
Bryan Gordon <bryanbge92@gmail.com>
2026-01-15 19:45:14 -0500

    Add methods for length unit converser

--- convert-units/routes/convertRoutes.go
+++ convert-units/routes/convertRoutes.go
@@ -1,1 +1,29 @@
 package routes
+
+/* Milimeters conversions */
+
+func mmTocm() {
+
+}
+
+func mmTometer() {
+
+}
+
+func mmToKm() {
+
+}
+
+/* Centimeters conversions */
+
+func cmTomm() {
+
+}
+
+func cmTometer() {
+
+}
+
+func cmTokm() {
+
+}
