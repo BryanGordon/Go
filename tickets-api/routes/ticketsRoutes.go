043db48a5413f01d90e4433314e691930ad90cf6
Bryan Gordon <bryanbge92@gmail.com>
2026-02-03 21:00:01 -0500

    Update function for generate concert tickets

--- tickets-api/routes/ticketsRoutes.go
+++ tickets-api/routes/ticketsRoutes.go
@@ -1,6 +1,41 @@
 package routes
 
-func GenerateTicket() {
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"ticket-api/models"
+
+	"github.com/gorilla/mux"
+)
+
+var filename string = "tickets.json"
+
+func GenerateTicketConcert(res http.ResponseWriter, req *http.Request) {
+	var ticketList []models.Ticket
+	params := mux.Vars(req)
+
+	_, err := os.Stat(filename)
+
+	if err == nil {
+		data, err := os.ReadFile(filename)
+
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			res.Write([]byte("File could not read"))
+			return
+		}
+
+		_ = json.Unmarshal(data, &ticketList)
+		newTicket := models.Ticket{Id: params["id"], Type: "Concert", Number: 1231}
+		ticketList = append(ticketList, newTicket)
+	}
+
+	res.WriteHeader(http.StatusBadRequest)
+	res.Write([]byte("File not found"))
+}
+
+func GenerateTicketMovie(res http.ResponseWriter, req *http.Request) {
 
 }
 
