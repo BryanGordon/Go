0cc089706ebb1c1b1c482e6410717f85855828e9
Bryan Gordon <bryanbge92@gmail.com>
2026-02-20 21:13:31 -0500

    Add cors restrictions

--- tickets-api/main.go
+++ tickets-api/main.go
@@ -4,10 +4,17 @@ import (
 	"log"
 	"net/http"
 	"ticket-api/routes"
+
+	"github.com/gorilla/handlers"
 )
+
+var allowedAccess = []string{"http://localhost:5173"}
+var allowedMethods = []string{"GET", "POST", "OPTIONS"}
+var allowedHeaders = []string{"Content-Type"}
 
 func main() {
 	data := routes.Routes()
 
-	log.Fatal(http.ListenAndServe(":3000", data))
+	log.Fatal(http.ListenAndServe(":3000",
+		handlers.CORS(handlers.AllowedOrigins(allowedAccess), handlers.AllowedMethods(allowedMethods), handlers.AllowedHeaders(allowedHeaders))(data)))
 }
