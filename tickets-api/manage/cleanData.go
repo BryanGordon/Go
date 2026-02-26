90ca8cf0c16d7afb3edac4f9a043582e1d9c1880
Bryan Gordon <bryanbge92@gmail.com>
2026-02-25 21:16:34 -0500

    Add method for delete json data

--- tickets-api/manage/cleanData.go
+++ tickets-api/manage/cleanData.go
@@ -1,3 +1,43 @@
+package manage
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"ticket-api/models"
+	"time"
+)
+
+var filename = "tickets.json"
+
+func deleteData() {
+	_, err := os.Stat(filename)
+	var allData models.TicketAvailable
+
+	if err == nil {
+		data, err := os.ReadFile(filename)
+
+		if err != nil {
+			return
+		}
+		_ = json.Unmarshal(data, &allData)
+
+		allData.Concert = allData.Concert[:0]
+		allData.Movie = allData.Concert[:0]
+		allData.Train = allData.Train[:0]
+
+		newJson, err := json.MarshalIndent(allData, " ", " ")
+
+		if err != nil {
+			return
+		}
+
+		err = os.WriteFile(filename, newJson, 0644)
+
+		return
+	}
+
+}
 
 func CleanDataFile(ctx context.Context) {
 	timer := time.NewTicker(5 * time.Minute)
