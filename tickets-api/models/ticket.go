df0d62aec8c4a744d8a1125395b425180665bd21
Bryan Gordon <bryanbge92@gmail.com>
2026-03-02 19:00:24 -0500

    Change id data type to uuid

--- tickets-api/models/ticket.go
+++ tickets-api/models/ticket.go
@@ -1,7 +1,9 @@
 package models
 
+import "github.com/gofrs/uuid"
+
 type Ticket struct {
-	Id     string `json:"id"` // cambiar a uuid
-	Type   string `json:"type"`
-	Number string `json:"number"`
+	Id     uuid.UUID `json:"id"`
+	Type   string    `json:"type"`
+	Number string    `json:"number"`
 }
