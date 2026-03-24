b1bba136d4895ddc9b77783f0495b5f1db1f9a35
Bryan Gordon <bryanbge92@gmail.com>
2026-03-23 21:27:46 -0500

    Add new ticket attribute

--- tickets-api/models/ticket.go
+++ tickets-api/models/ticket.go
@@ -3,7 +3,8 @@ package models
 import "github.com/gofrs/uuid"
 
 type Ticket struct {
-	Id     uuid.UUID `json:"id"`
-	Type   string    `json:"type"`
-	Number string    `json:"number"`
+	Id        uuid.UUID `json:"id"`
+	Type      string    `json:"type"`
+	Number    string    `json:"number"`
+	Validated bool      `json:"validated"`
 }
